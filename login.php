076743acec5384d735cf1bc9ee8e50ef00dd9f86
Erik-Humberto <erikcerecero2002@gmail.com>
2023-06-05 00:54:01 -0600

    primer comit

---
+++ login.php
@@ -0,0 +1,38 @@
+
+<?php
+session_start();
+require_once 'conexion.php';
+
+if (isset($_POST['enviar'])){
+
+  $correo_electronico=$_POST['correo_electronico'];
+  $contrasena=$_POST['contrasena'];
+  
+ 
+  $sql=$cnnPDO->prepare('SELECT * from usuarios WHERE correo_electronico=:correo_electronico AND contrasena=:contrasena');
+  $sql->bindParam(':correo_electronico',$correo_electronico);
+  $sql->bindParam(':contrasena',$contrasena);
+  $sql->execute();
+  $count = $sql->rowCount();
+
+  if ($count==1) {
+    $query=$cnnPDO->prepare('SELECT * from usuarios WHERE correo_electronico=:correo_electronico');
+    $query->bindParam(':correo_electronico',$correo_electronico);
+    $query->execute();
+    $row=$query->fetch(PDO::FETCH_ASSOC);
+    $rol=$row['roles'];
+    $id_usuario=$row['id_usuario'];
+
+    if ($rol=="admin"){
+      $_SESSION['id_usuario'] = $id_usuario;
+      echo"<script>window.location='admin.php'</script>";
+    }else if ($rol=="usaurio"){
+      $_SESSION['id_usuario'] = $rol;
+      echo"<script>window.location='index.html'</script>";
+    }
+
+  }else{
+    header("location:login.html?mensaje=Correo o contraseña incorrectos");
+  }
+}
+?>
